bdbfb46a91654849a9e24807e9151dd342e268a9
Igor <i.ergakoff@mail.ru>
2023-06-17 14:00:18 +0300

    Algorithms. Homework 3

---
+++ Homevork_3/Homework_3.java
@@ -0,0 +1,19 @@
+public class Homework_3 {
+
+    public static void main(String[] args) {
+        
+        LinkedListT <Integer> ListT = new LinkedListT<>();
+
+        ListT.add(5);
+        ListT.add(1);
+        ListT.add(8);
+        ListT.add(15);
+        ListT.add(0);
+        ListT.add(9);
+        ListT.add(7);
+
+        ListT.print();
+        ListT.revers();
+        ListT.print();
+    }
+}
